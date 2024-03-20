e7b4f6d205b2b037ad1744cfc7d9e7cead8a70db
zakir <zakir95olymp@gmail.com>
2024-03-20 11:22:57 +0900

    Updated urls.py in app

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import RegisterAPIView, EmailVerificationAPIView, LoginAPIView
+from .views import RegisterAPIView, EmailVerificationAPIView, LoginAPIView, ResendActivationEmailAPIView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenBlacklistView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('resend-activation-email/', ResendActivationEmailAPIView.as_view(), name='resend_activation_email'),
     path('email-activate/', EmailVerificationAPIView.as_view(), name='email-verification'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
